bf77eb1004295c142ec6cbf01d962a166956ce74
samuel <samuelkelvinmwangi@gmail.com>
2023-07-24 15:56:18 +0300

    active records

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,10 @@
-class Dog
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'db/students.db'
+)
+
+class Dog < ActiveRecord::Base
 end
